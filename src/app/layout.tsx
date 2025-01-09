8ed41dee3ed96fe395948a8fb4a57717cd028fd6
Marcelo Esparza <maresparzam@gmail.com>
2025-01-09 16:43:19 -0500

    Setup project with nextJs

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Marcelo Esparza website",
+  description: "",
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
